3866b455823cf3549a75cea3c9385aefdc70c0a3
Javier <javierdwd@gmail.com>
2019-05-14 01:28:02 -0300

    Implementar un IntersectObserver en en panel de actividad de GitHub

--- src/scripts/GithubWidget.js
+++ src/scripts/GithubWidget.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GithubActivity from 'jv_code_github_activity';
 
-ReactDOM.render(
-  React.createElement(GithubActivity, {
-    login: 'javierdwd'
-  }),
-  document.getElementById('github-activity-root')
-);
+let widgetContainer = document.getElementById('github-activity-root');
+
+function initWidget(node) {
+  ReactDOM.render(
+    React.createElement(GithubActivity, {
+      login: 'javierdwd'
+    }),
+    node
+  );
+};
+
+if (window.IntersectionObserver) {
+  const io = new IntersectionObserver(entries =>{
+    let widget = Array.from(entries).find(x => x.target === widgetContainer);
+
+    if(widget && widget.isIntersecting === true) {
+      initWidget(widgetContainer);
+
+      io.unobserve(widgetContainer);
+      io.disconnect();
+    }
+  });
+
+  io.observe(document.getElementById('github-activity-root'));
+  io.observe(document.querySelector('.c-avatar'));
+} else {
+  initWidget(widgetContainer);
+}
+
