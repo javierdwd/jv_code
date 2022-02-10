16b27dab631bac2da4babeb9bae390393e362d99
Javier Van Breedan <javierdwd@gmail.com>
2022-02-10 12:14:43 -0300

    Add Vite project scaffold

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <div class="l-page-wrapper">
+      <main class="l-content">
+        {/* <module href="/profile.html"></module>
+
+        <module href="/links.html"></module>
+
+        <module href="/github_activity.html"></module> */}
+      </main>
+    </div>
+  );
+}
