4a6c50642862d1d8959b6d84e8526eec23c24e40
Javier Van Breedan <javierdwd@gmail.com>
2022-02-11 21:52:11 -0300

    Restore "darken-bg" effect

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import { App } from './app'
 import './components/CompactList.js'
 import './styles/main.css'
 
+if(!window.customElements) {
+  document.body.classList.add("ce-fallback");
+}
+
 render(<App />, document.getElementById('app')!)
