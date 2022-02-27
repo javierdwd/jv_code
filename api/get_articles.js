3ef5ec138f0bf4ccb29f2988dc57043043d14287
Javier Van Breedan <javierdwd@gmail.com>
2022-02-26 21:02:49 -0300

    Add "get_articles" Lambda function

--- api/get_articles.js
+++ api/get_articles.js
@@ -1,7 +1,54 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+const https = require('https')
+
+
+export default function handler(_, response) {
+  let responseRaw = '';
+
+  const data = JSON.stringify({
+    consumer_key: process.env.POCKET_CONSUMER_KEY,
+    access_token: process.env.POCKET_ACCESS_TOKEN,
+    tag: "sport",
+    sort: "newest"
   });
+
+  const req = https.request({
+    hostname: 'getpocket.com',
+    port: 443,
+    path: '/v3/get',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF8',
+      'Content-Length': data.length
+    }
+  }, res => {
+    res.on('data', data => {
+      responseRaw += data;
+    });
+
+    res.on('end', () => {
+      const jsonResponse = JSON.parse(responseRaw)
+
+      if(jsonResponse.error) {
+        response.status(500).json({
+          message: "Unexpected GetPocket's response error",
+        });
+      } else {
+        response.status(200).json(Object.values(jsonResponse.list).map(el => ({
+          item_id: el.item_id,
+          url: el.given_url || el.resolved_urls,
+          title: el.given_title || el.resolved_title,
+          added: el.time_added,
+        })));
+      }
+    })
+  });
+
+  req.on('error', error => {
+    response.status(500).json({
+      message: error.message
+    });
+  });
+
+  req.write(data)
+  req.end();
 }
